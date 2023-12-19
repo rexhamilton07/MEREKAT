c18bf5ba4d080e026f411167814a1d270ba3b943
Rex Hamilton <>
2023-12-18 18:01:45 -0700

    font edits again

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
       </br>
       <p className = "bio">
         MEREKAT is the brainchild of Sebastian and Lucas Brabant, Andreas Fauquier and Rex Hamilton. Founded in 2021, the bandmates met as students at McGill University and pulled together what instruments they could find in a friend's moldy basement to begin crafting music that draws inspiration from the energy of The Strokes, the melodies of Arctic Monkeys, and the authenticity of Oasis.
-        <br></br><br></br>Over the last few years, the intimate venues of Montréal have served as both their workshop and canvas, where the band experiments with the infectious energy that defines their live performances and lyrics on the elation and torment of contemporary youth. 
+        <br></br><br></br>Over the last few years, the intimate venues of Montréal have served as both their workshop and canvas, where the band experiments with the infectious energy that defines their live performances. 
         In recent months, the band has begun to release their work, beginning with debut single "Another One" in August and follow-up "talking thru a smile" in November, the latter of which was featured on Spotify's editorial playlist <i>Fresh Finds Canada</i>. 
         The band is fortunate to have received funding from FACTOR Canada, which is going towards their debut EP to be released mid-2024. 
       </p>
